940d20ba223edcf9779eb6c92a40a1a46c9917fe
Yoann Delion <yoanndelion@gmail.com>
2020-06-29 22:29:46 +0200

    Express Middlewares example

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const { response } = require('express')
 
 const app = express()
 
-app.use((request, response) => {
-    response.json({ message: 'Server response from express' })
+app.use((request, response, next) => {
+    console.log('Request received')
+    next()
+})
+
+app.use((request, response, next) => {
+    response.status(201)
+    next()
+})
+
+app.use((request, response, next) => {
+    response.json({ message: 'Request well received' })
+    next()
+})
+
+app.use((request, response, next) => {
+    console.log('Response send successfully')
 })
 
 module.exports = app
