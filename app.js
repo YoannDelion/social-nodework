69a16bce5bc52f2db5c7d8c8de839a12627bcd1f
Yoann Delion <yoanndelion@gmail.com>
2020-06-30 22:31:07 +0200

    MongoDB config

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 // Extract JSON Object from requests
 const bodyParser = require('body-parser')
+// Mongoose helps communicate with MongoDB database
+const mongoose = require('mongoose')
 
 const app = express()
+
+// MongoDB connection config
+mongoose.connect('mongodb+srv://yoanndelion:sWMUZ8bxdhiWb4RE@social-nodework.i7v5o.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Handle CORS error
 app.use((request, response, next) => {
