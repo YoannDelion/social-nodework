f25f91c5b2921b87377e62ae9f147cfeba5a8ba0
Yoann Delion <yoanndelion@gmail.com>
2020-07-13 22:31:29 +0200

    Created signup function

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,23 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 /**
- * Sign up a user
+ * Hash password and then sign up a user    
  */
-exports.signup = (request, response, next) => { }
+exports.signup = (request, response, next) => {
+    bcrypt.hash(request.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                ...request.body,
+                password: hash
+            })
+
+            user.save()
+                .then(() => response.status(201).json({ message: 'User created!' }))
+                .catch(error => response.status(400).json({ error }))
+        })
+        .catch(error => response.status(500).json({ error }))
+}
 
 /**
  * Log a user in 
